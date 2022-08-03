a877791bcaadd95498d93c02005955111b95752f
Guilherme Santos <guuilhermessantos@gmail.com>
2022-08-03 13:46:33 -0300

    dev finally

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedBackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading";
 import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
@@ -20,16 +22,25 @@ export function FeedbackContentStep({
 
 	const [screenshot, setScreenshot] = useState<string | null>(null)
 	const [comment, setComment] = useState('')
+	const [isSendingFeedback, setIsSendingFeedback] = useState(false)
+
+
 	const feedbackTypeInfo = feedBackTypes[feedbackType]
 
-	function handleSubmitFeedback(event: FormEvent) {
-		event.preventDefault()
-		console.log('screenshot, comment', screenshot, comment)
+	async function handleSubmitFeedback(event: FormEvent) {
+		event.preventDefault();
 
+		setIsSendingFeedback(true)
+
+		await api.post('/feedbacks', {
+			type: feedbackType,
+			comment,
+			screenshot,
+		})
+
+		setIsSendingFeedback(false)
 		onFeedbackSent()
 	}
-
-
 
 	return (
 		<>
@@ -51,7 +62,7 @@ export function FeedbackContentStep({
 			<form onSubmit={handleSubmitFeedback} className="my-4 w-full">
 				<textarea
 					className="min-w-[304px] w-full min-h-[122px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-					placeholder="Conte com detalhes oque está acontecendo..."
+					placeholder={feedbackTypeInfo.placeholder}
 					onChange={event => setComment(event.target.value)}
 				/>
 				<footer className="flex gap-2 mt-2">
@@ -61,10 +72,10 @@ export function FeedbackContentStep({
 					/>
 					<button
 						type="submit"
-						disabled={comment.length === 0}
+						disabled={comment.length === 0 || isSendingFeedback}
 						className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus-ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
 					>
-						Enviar feedback
+						{isSendingFeedback ? <Loading /> : 'Enviar feedback'}
 					</button>
 				</footer>
 			</form>
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -13,21 +13,26 @@ export const feedBackTypes = {
       image: {
          source: bugImageUrl,
          alt: 'Imagem de um Inseto',
-      }
+      },
+      placeholder:
+      "Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo...",
    },
    IDEA: {
       title: 'Ideia',
       image: {
          source: ideaImageUrl,
          alt: 'Imagem de uma lâmpada',
-      }
+      },
+      placeholder:
+      "Teve uma ideia de melhoria ou de nova funcionalidade? Conta pra gente!",
    },
    OTHER: {
       title: 'Outro',
       image: {
          source: thoughtImageUrl,
          alt: 'Imagem de um balão de pensamento',
-      }
+      },
+      placeholder: "Queremos te ouvir. O que você gostaria de nos dizer?",
    },
 }
 export type FeedbackType = keyof typeof feedBackTypes

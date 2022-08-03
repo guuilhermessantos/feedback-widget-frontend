eacb7fd2b0c6584d358c17403c64a2873eb75477
Guilherme Santos <guuilhermessantos@gmail.com>
2022-08-03 15:09:06 -0300

    dev finally

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,7 +1,7 @@
 import { CloseButton } from "../CloseButton";
-import bugImageUrl from '../../assets/bug.svg'
-import ideaImageUrl from '../../assets/idea.svg'
-import thoughtImageUrl from '../../assets/thought.svg'
+import bugImageUrl from '../../assets/Bug.svg'
+import ideaImageUrl from '../../assets/Idea.svg'
+import thoughtImageUrl from '../../assets/Thought.svg'
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
